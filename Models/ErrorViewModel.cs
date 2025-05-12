81a16a0ec00c07eaf884389c7f147f608bc6e225
ANGELDANIELCX <angeldaniel.ramirzhernadez.gen23@cbtis105.edu.mx>
2025-05-12 08:59:23 -0600

    Proyecto inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ConsultasVuelosLA.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
